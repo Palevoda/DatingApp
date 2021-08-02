c33633e51b97d200df30f3c411ab4fbaf59d292e
Aliaksandr-Palevoda <aliaksandr.palevoda@gmail.com>
2021-08-02 19:39:22 +0300

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -10,22 +12,25 @@ export class AppComponent implements OnInit {
   title = 'The dating app';
   users: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private accountService : AccountService) {
     
   }
 
   ngOnInit()
   {
-      this.getUsers();
+      // this.getUsers(); 1
+      this.setCurrentUser();
   }
 
-  getUsers()
-  {
-    this.http.get("https://localhost:44311/api/users").subscribe(
-      responce => {this.users = responce}, 
-      error => {console.log(error);
-      })    
+  setCurrentUser(){
+    const user : User = JSON.parse(localStorage.getItem('user') || '{}'); //&&
   }
 
-
+  // getUsers() 1
+  // {
+  //   this.http.get("https://localhost:5001/api/users").subscribe(
+  //     responce => {this.users = responce}, 
+  //     error => {console.log(error);
+  //     })    
+  // }
 }
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+
+export class NavComponent implements OnInit {
+  model: any = {}
+  //loggedIn: boolean = false; //&&
+ // currentUser$: Observable<User> = this.accountService.currentUser$; //&
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+     this.accountService.getCurrentUser();
+    // this.currentUser$ = this.accountService.currentUser$; //&
+    // console.log(this.currentUser$);
+  }
+
+  login() { 
+    this.accountService.login(this.model).subscribe(
+      response =>
+      {
+        console.log(response);
+      },
+      error => {console.log(error)}
+      );
+  }
+
+  logout()
+  {
+    this.accountService.logout();
+  }
+
+}
